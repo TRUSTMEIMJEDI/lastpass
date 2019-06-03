abe77a5b094ab976bbd82a5f39cefbb2547a47f1
Marcin Friedrich <fredros1996@gmail.com>
2019-06-04 01:41:48 +0200

    Added Field adding and editing

--- src/sample/controller/FieldInfoController.java
+++ src/sample/controller/FieldInfoController.java
@@ -1,21 +1,89 @@
 package sample.controller;
 
-import sample.view.res.ImageManager;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import sample.model.Account;
+import sample.model.Field;
+import sample.repository.FieldRepository;
+import sample.repository.IFieldRepository;
 
-public class FieldInfoController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class FieldInfoController implements Initializable {
     AppController appController;
+    IFieldRepository fieldRepository;
 
-    void bindParent(AppController appController) {
-        this.appController = appController;
+    private Field fieldToEdit;
+    private Field newField;
+    private boolean update = false;
 
-        ImageManager im = appController.getImageManager();
+    @FXML
+    TextField tfName;
+    @FXML
+    TextField tfDomain;
+    @FXML
+    TextArea taNotes;
+    @FXML
+    Button bOk;
+    @FXML
+    Button bCancel;
+    @FXML
+    Label fieldLabel;
 
-// Buttons from FieldInfo.fxml
-//        bGenD.setGraphic(im.constructImageViewFrom(ImageManager.ICON_REFRESH, 16, 16, true));
-//        bAccD.setGraphic(im.constructImageViewFrom(ImageManager.ICON_SHARE, 16, 16, true));
-//        bModI.setGraphic(im.constructImageViewFrom(ImageManager.ICON_EDIT, 16, 16, true));
-//        bSupI.setGraphic(im.constructImageViewFrom(ImageManager.ICON_REMOVE, 16, 16, true));
-//        bCopN.setGraphic(im.constructImageViewFrom(ImageManager.ICON_COPY, 16, 16, true));
-//        bSupN.setGraphic(im.constructImageViewFrom(ImageManager.ICON_REMOVE, 16, 16, true));
+
+    public void btnOk() {
+        if (update) {
+            fieldToEdit.setName(tfName.getText());
+            fieldToEdit.setData(tfDomain.getText());
+            fieldToEdit.setNotes(taNotes.getText());
+            fieldRepository.updateField(fieldToEdit);
+        } else {
+            newField = new Field();
+            List<Account> accounts = new ArrayList<>();
+            newField.setName(tfName.getText());
+            newField.setData(tfDomain.getText());
+            newField.setNotes(taNotes.getText());
+            newField.setAccounts(accounts);
+            fieldRepository.createField(newField);
+        }
+        appController.unMountFieldView();
+    }
+
+    public void btnCancel() {
+        appController.unMountFieldView();
+    }
+
+    void initFieldInfo(Field field) {
+        update = true;
+        fieldToEdit = field;
+        fieldLabel.setText(field.getName());
+        tfName.setText(field.getName());
+        tfDomain.setText(field.getData());
+        taNotes.setText(field.getNotes());
+    }
+
+    void createNewField() {
+        update = false;
+        fieldLabel.setText("Add new field");
+        tfName.setText("");
+        tfDomain.setText("");
+        taNotes.setText("");
+    }
+
+    void bindParent(AppController c) {
+        appController = c;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fieldRepository = new FieldRepository();
     }
 }
